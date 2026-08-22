5dce71138bc9bd7694b7edb30dd52258e6ece173
Kurt Hamm <kurt@hamm.me>
2026-08-22 09:25:16 -0400

    Add synthetic MIDI test package

---
+++ src/openorchestrion/testing/__init__.py
@@ -0,0 +1,5 @@
+"""Synthetic, copyright-clean test utilities for OpenOrchestrion."""
+
+from .midi_fixtures import GeneratedFixture, generate_suite
+
+__all__ = ["GeneratedFixture", "generate_suite"]
