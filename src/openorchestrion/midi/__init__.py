531ba5dca4515f72091f5fa7ce11c67e94ee5bfc
Kurt Hamm <kurt@hamm.me>
2026-08-24 13:27:55 -0400

    Expose General MIDI program resolution

--- src/openorchestrion/midi/__init__.py
+++ src/openorchestrion/midi/__init__.py
@@ -1,6 +1,15 @@
-"""MIDI analysis, routing, and playback support for OpenOrchestrion."""
+"""MIDI analysis, General MIDI vocabulary, routing, and playback support."""
 
 from .analysis_models import MidiAnalysis
 from .analyzer import analyze_midi, analyze_midifile
+from .gm import GM_PROGRAM_NAMES, GMProgramError, gm_program_name, resolve_gm_program
 
-__all__ = ["MidiAnalysis", "analyze_midi", "analyze_midifile"]
+__all__ = [
+    "MidiAnalysis",
+    "analyze_midi",
+    "analyze_midifile",
+    "GM_PROGRAM_NAMES",
+    "GMProgramError",
+    "gm_program_name",
+    "resolve_gm_program",
+]
